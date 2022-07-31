7969493db4ad12da4e85c34d6ee11e70918e2f12
Ranjithkumar <selvjith@gmail.com>
2022-07-31 12:09:11 +0200

    asdf

--- main.tf
+++ main.tf
@@ -32,10 +32,4 @@ resource "google_compute_instance" "default" {
   metadata = {
     name = "terraform"
   }
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
 }
